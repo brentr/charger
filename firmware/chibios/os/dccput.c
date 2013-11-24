f295bbbb29d9686673f02b81b22535352069d9ac
Brent Roman <brent@mbari.org>
2013-11-23 21:10:59 -0800

    whitespace fix in comment

--- firmware/chibios/os/dccput.c
+++ firmware/chibios/os/dccput.c
@@ -5,7 +5,7 @@
  *   spen@spen-soft.co.uk                                                  *
  *   Copyright (C) 2008 by Frederik Kriewtz                                *
  *   frederik@kriewitz.eu                                                  *
- *   revised by:  brent@mbari.org  10/27/13                                  *
+ *   revised by:  brent@mbari.org  10/27/13                                *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
