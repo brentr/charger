ca351251da3d739f4d737d0e1dacbe885183445f
Brent Roman <brent@mbari.org>
2013-11-23 21:12:21 -0800

    ADC triggered by TIM6 at 200hz

--- firmware/zev/main.c
+++ firmware/zev/main.c
@@ -1,6 +1,7 @@
 #include <ch.h>
 #include <hal.h>
 #include <chprintf.h>
+#include <stm32_tim.h>
 
 #include "debugput.h"
 
@@ -51,15 +52,52 @@ static adcsample_t samples[ADC_GRP1_NUM_CHANNELS * ADC_GRP1_BUF_DEPTH];
  * Channels:    IN10, IN11, IN12   (16 cycles sample time)
  */
 #define adcSampleTime  ADC_SAMPLE_16
+ //Timer 6 trigger
+#define adcTrigger (ADC_CR2_EXTSEL_1|ADC_CR2_EXTSEL_3 | ADC_CR2_EXTEN_0)
+#define adcTimer STM32_TIM6
+#define adcTimerClkRate  STM32_PCLK1
+#define enableAdcTimer() rccEnableAPB1(RCC_APB1ENR_TIM6EN, FALSE)
+#define disableAdcTimer() rccDisableAPB1(RCC_APB1ENR_TIM6EN, FALSE)
+
+static unsigned totalSamples = 0, totalErrs = 0, count = 0;
+
+static void adcDone(ADCDriver *adcp, adcsample_t *buffer, size_t n)
+{
+  totalSamples += n;
+}
+
+static void adcErr(ADCDriver *adcp, adcerror_t err)
+{
+  totalErrs++;
+}
+
+static INLINE void setAdcTimebase(uint32_t hz)
+{
+  adcTimer->PSC = (uint16_t)((adcTimerClkRate / hz) - 1);
+}
+
+/**
+ * @brief   Starts the timer in continuous mode.
+ */
+static INLINE void startAdcTimer(uint16_t interval) {
+
+  stm32_tim_t *tim = adcTimer;
+  tim->ARR   = interval - 1;              /* Time constant.           */
+  tim->EGR   = STM32_TIM_EGR_UG;          /* Update event.            */
+  tim->CNT   = 0;                         /* Reset counter.           */
+  tim->DIER  = 0;        /* no interrupts enabled */
+  tim->CR2   = STM32_TIM_CR2_MMS(2);      /* output TRGO pulse on update  */
+  tim->CR1   = STM32_TIM_CR1_URS | STM32_TIM_CR1_CEN;
+}
 
 static const ADCConversionGroup adcgrpcfg = {
-  FALSE,
+  TRUE,
   ADC_GRP1_NUM_CHANNELS,
-  NULL,
-  NULL,
+  adcDone,
+  adcErr,
   /* HW dependent part.*/
   0,                        /* CR1 */
-  ADC_CR2_SWSTART,          /* CR2 */
+  adcTrigger,               /* CR2 -- trigger */
   0,
   ADC_SMPR2_SMP_AN10(adcSampleTime) | ADC_SMPR2_SMP_AN11(adcSampleTime) |
    ADC_SMPR2_SMP_AN12(adcSampleTime),
@@ -71,7 +109,6 @@ static const ADCConversionGroup adcgrpcfg = {
   ADC_SQR5_SQ1_N(ADC_CHANNEL_IN10) | ADC_SQR5_SQ2_N(ADC_CHANNEL_IN11) |
   ADC_SQR5_SQ3_N(ADC_CHANNEL_IN12)
 };
-
 
 int main(void) {
   halInit();
@@ -103,17 +140,25 @@ int main(void) {
   configurePad(BUZZER, PAL_MODE_OUTPUT_OPENDRAIN);
 
   /*
-   * Initializes the ADC driver 1
-   */
-  configureGroup(ANALOGINS, PAL_MODE_INPUT_ANALOG);
-  adcStart(&ADCD1, NULL);
-
-  /*
    * Enable DAC channel 1 on PA4
    */
   configureGroup(ANALOGOUTS, PAL_MODE_INPUT_ANALOG);
   RCC->APB1ENR |= RCC_APB1ENR_DACEN;
   DAC->CR = DAC_CR_EN1;
+
+  /*
+   * Initializes the ADC driver 1
+   */
+  configureGroup(ANALOGINS, PAL_MODE_INPUT_ANALOG);
+  adcStart(&ADCD1, NULL);
+  adcStartConversion(&ADCD1, &adcgrpcfg, samples, ADC_GRP1_BUF_DEPTH);
+
+  /*
+   * Configure Adc Timer to output triggers at 200hz
+   */
+  enableAdcTimer();
+  setAdcTimebase(1000);  //1 ms timer tics  
+  startAdcTimer(5);
 
   while (1) {
     int key;
@@ -138,25 +183,24 @@ int main(void) {
 
     setPad(GREEN_LED);
     setPad(BUZZER);
-    chThdSleepMilliseconds(30);
+    chThdSleepMilliseconds(50);
     clearPad(GREEN_LED);
     clearPad(BUZZER);
-    chThdSleepMilliseconds(35);
+    chThdSleepMilliseconds(50);
 
-    msg_t err = adcConvert(&ADCD1, &adcgrpcfg, samples, ADC_GRP1_BUF_DEPTH);
-    if (!err) {
-      adcsample_t avg_ch1, avg_ch2, avg_ch3;
+    adcsample_t avg_ch1, avg_ch2, avg_ch3;
 
-      /* Calculates the average values from the ADC samples.*/
-      avg_ch1 = (samples[0] + samples[3] + samples[6] + samples[9]) / 4;
-      avg_ch2 = (samples[1] + samples[4] + samples[7] + samples[10]) / 4;
-      avg_ch3 = (samples[2] + samples[5] + samples[8] + samples[11]) / 4;
+    /* Calculates the average values from the ADC samples.*/
+    avg_ch1 = (samples[0] + samples[3] + samples[6] + samples[9]) / 4;
+    avg_ch2 = (samples[1] + samples[4] + samples[7] + samples[10]) / 4;
+    avg_ch3 = (samples[2] + samples[5] + samples[8] + samples[11]) / 4;
 
-      debugPrint("%s:Vcmd=%d,Vin=%d,VcmdIn=%d,Thres=%d",
-	  	 power, DAC->DOR1, avg_ch1, avg_ch2, avg_ch3);
-      chprintf(&SD1, "%s: Vcmd=%d, Vin=%d, VcmdIn=%d, Thres=%d\r\n",
-	  	 power, DAC->DOR1, avg_ch1, avg_ch2, avg_ch3);
-    }else
-      debugPrint("adcConvert returned err #%d", err);
+    chprintf(&SD1, "#%d:%s: Vcmd=%d, Vin=%d, VcmdIn=%d, Thres=%d\r\n",
+	 totalSamples, power, DAC->DOR1, avg_ch1, avg_ch2, avg_ch3);
+    if (count++ >= 5) {
+      debugPrint("#%d:%s:Vcmd=%d,Vin=%d,VcmdIn=%d,Thres=%d (%d errs)",
+	 totalSamples, power, DAC->DOR1, avg_ch1, avg_ch2, avg_ch3, totalErrs);
+      count = 0;
+    }
   }
 }
